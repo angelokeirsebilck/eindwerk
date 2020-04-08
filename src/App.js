3b21d9f75c789f72497a3062feb4f1dd8ab1968d
Angelo Keirsebilck <angelo@digitalpulse.be>
2020-04-08 10:03:06 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import NavBar from './components/Nav/NavBar';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Page404 from './pages/Page404';
+import { API, TOKEN } from "./config/API";
+import Login from './pages/Login';
+import { connect } from 'react-redux';
+import { loginAction, getUserData } from './redux/actions/authActions';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  componentDidMount() {
+    if (TOKEN) {
+      // Als er een token is (uit local storage) dan gaan we de gebruikersgevens ophalen
+      getUserData();
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route path="/register" component={Register}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/" component={Page404}></Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+const MapStateToProps = (state) => {
+  return {
+      user: state.auth.user
+  }
+}
+
+const MapDispatchToProps = (dispatch) => {
+  return {
+    loginAction: (response) => dispatch(loginAction(response))
+  }
+}
+
+export default connect(MapStateToProps,MapDispatchToProps)(App);
