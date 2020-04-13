6bd1615981c37c1530252b30ee41bad0a0ddf780
Angelo Keirsebilck <angelo@digitalpulse.be>
2020-04-13 08:53:00 +0200

    Add comment component

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classes from './Comment.module.css';
 
 export default class Comment extends Component {
     render() {
+        const {body, user, created_at} = this.props;
+        const date = new Date(created_at);
+        const dateFormat = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
         return (
-            <div>
-
+            <div className={classes.Comment}>
+                {body}
             </div>
         )
     }
 }
 
 Comment.propTypes = {
-
+    body: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
+    user: PropTypes.object.isRequired
 };
