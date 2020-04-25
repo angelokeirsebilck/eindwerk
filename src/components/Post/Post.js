c3d0ed98cf3aa0e7dabb46f0ce7605a6afea2e67
Angelo Keirsebilck <angelo@digitalpulse.be>
2020-04-25 15:02:03 +0200

    Changed post css + layout

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -12,11 +12,11 @@ export default class Post extends Component {
         const date = new Date(created_at);
         const dateFormat = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
         const link = "/posts/" + id;
+        const profileLink = '/profile/' + user.id;
         return (
-            <Link to={link}>
+            <Link to={link} className={classes.Post_link}>
                 <div className={classes.Post}>
-                    <div className={classes.Post_date}>Created at: {dateFormat}</div>
-                    <div className={classes.Post_date}>By: {user.first_name}</div>
+                    <div className={classes.Post_date}>Posted by <Link to={profileLink} className={classes.Post_username}> <span > {user.first_name}</span></Link> at {dateFormat}</div>
                     <div className={classes.Post_title}>{title}</div>
                     <div className={classes.Post_body}>
                         <Truncate lines={2}>{body}</Truncate>
