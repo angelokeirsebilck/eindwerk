d547ceb1e73628963ca7f50ad9221116f789ccf9
Angelo Keirsebilck <angelo@digitalpulse.be>
2020-04-25 15:02:23 +0200

    Add font family to app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import { API, TOKEN } from "./config/API";
 import Login from './pages/Login';
 import { connect } from 'react-redux';
 import { loginAction, setUserData } from './redux/actions/authActions';
+import PostDetails from './pages/PostDetails';
+import AddPost from './pages/AddPost';
+import Profile from './pages/Profile';
+import classes from './App.module.css';
 
 class App extends Component {
 
@@ -22,12 +26,15 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <Router>
           <NavBar />
           <Switch>
             <Route path="/register" component={Register}></Route>
             <Route path="/login" component={Login}></Route>
+            <Route path="/posts/:post" component={PostDetails}></Route>
+            <Route path="/addpost" component={AddPost}></Route>
+            <Route path="/profile/:userId" component={Profile}></Route>
             <Route path="/" exact component={Home}></Route>
             <Route path="/" component={Page404}></Route>
           </Switch>
