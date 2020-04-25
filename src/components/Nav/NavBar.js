d9b32112e71a0a27ee7bcc9c6b0237ed1628596f
Angelo Keirsebilck <angelo@digitalpulse.be>
2020-04-25 14:58:15 +0200

    Changed navbar layout + mobile

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -4,13 +4,28 @@ import Button from '../Button/Button';
 import { connect } from 'react-redux';
 import { logoutAction } from '../../redux/actions/authActions';
 import { Link } from 'react-router-dom';
+import $ from 'jquery';
+import './Nav.css';
 
 class NavBar extends Component {
+
+    onHamburgerClickHandler = () => {
+        let hamburger = $('#hamburger');
+        let mobileBody = $('.Nav_mobile_body');
+        let body = $('body');
+        hamburger.toggleClass("open");
+        mobileBody.toggleClass("open");
+        body.toggleClass("open");
+    }
 
     render() {
         const containerStyle = {
             height: '100%',
             display: 'flex'
+        }
+
+        const containerStyleHeight = {
+            height: '100%'
         }
 
         let isLoggedIn = false;
@@ -22,25 +37,67 @@ class NavBar extends Component {
         return (
             <nav className={classes.Nav}>
                 <div className="container" style={containerStyle}>
-                    <Link to="/" className={classes.HomeLink}>HOME</Link>
+
+                    <Link to="/" className={classes.Home_link}>HOME</Link>
                     {isLoggedIn ?
-                        <ul className={classes.Navlist}>
-                            <li className={classes.Navitem}>
-                                <button onClick={this.props.logoutAction}>Logout</button>
+                        <ul className={classes.Nav_list}>
+                            <li className={classes.Nav_item}>
+                                <Button text={"Register"} link={"/register"} />
                             </li>
-                            <li>
-                                <Button text={"Profile"} link={"/profile"} />
+                            <li className={classes.Nav_item_dropdown}>
+                                <img className={classes.Nav_image} src={this.props.user.avatar} alt="" />
+                                <div className={classes.Nav_dropdown}>
+                                    <Button text={"AddPost"} link={"/addpost"} />
+                                    <button className={classes.Button_primary} onClick={this.props.logoutAction}>Logout</button>
+                                </div>
+                            </li>
+                            <li className={classes.Nav_mobile}>
+                                <div id="hamburger" onClick={this.onHamburgerClickHandler} className="Nav_hamburger">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
                             </li>
                         </ul>
                         :
-                        <ul className={classes.Navlist}>
-                            <li className={classes.Navitem}>
-                                <Button text={"Login"} link={"/login"} />
-                            </li>
-                            <li>
+                        <ul className={classes.Nav_list}>
+                            <li className={classes.Nav_item}>
                                 <Button text={"Register"} link={"/register"} />
                             </li>
+                            <li className={classes.Nav_item}>
+                                <Button text={"Login"} link={"/login"} />
+                            </li>
+                            <li className={classes.Nav_mobile}>
+                                <div id="hamburger" onClick={this.onHamburgerClickHandler} className="Nav_hamburger">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </li>
                         </ul>}
+                </div>
+                <div className="Nav_mobile_body">
+                    <div className="container" style={containerStyleHeight}>
+                        {isLoggedIn ?
+                            <ul className={classes.Nav_mobile_list}>
+                                <li className={classes.Nav_mobile_item}>
+                                    <Button text={"Register"} mobile={true} link={"/register"} />
+                                </li>
+                                <li><Button text={"AddPost"} mobile={true} link={"/addpost"} /></li>
+                                <li><button className={classes.Button_mobile} onClick={this.props.logoutAction}>Logout</button></li>
+                            </ul>
+                            :
+                            <ul className={classes.Nav_mobile_list}>
+                                <li className={classes.Navitem}>
+                                    <Button mobile={true} text={"Register"} link={"/register"} />
+                                </li>
+                                <li className={classes.Navitem}>
+                                    <Button mobile={true} text={"Login"} link={"/login"} />
+                                </li>
+                            </ul>}
+                    </div>
                 </div>
             </nav>
         )
@@ -54,9 +111,9 @@ const MapStateToProps = (state) => {
 
 const MapDispatchToProps = (dispatch) => {
     return {
-      logoutAction: () => dispatch(logoutAction())
+        logoutAction: () => dispatch(logoutAction())
     }
-  }
+}
 
 export default connect(MapStateToProps, MapDispatchToProps)(NavBar);
 
