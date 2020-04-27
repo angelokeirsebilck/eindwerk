10faccf9b4bcd08231b2805ff6e693c9b100867f
Angelo Keirsebilck <angelo@digitalpulse.be>
2020-04-27 06:44:37 +0200

    Change layout PostDetailsItem

--- src/components/Post/PostDetailsItem.js
+++ src/components/Post/PostDetailsItem.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import classes from './PostDetailsItem.module.css';
+import { Link } from 'react-router-dom';
 
 export default class PostDetailsItem extends Component {
+
     render() {
-        const { id, title, body, created_at, updated_at, user_id, comments_count, comments } = this.props;
+        const { title, body, created_at, user} = this.props;
+        const profileLink = '/profile/' + user.id;
         return (
             <div className={classes.Post}>
-                    <div className={classes.Post_date}>Created at: {created_at}</div>
-                    <div className={classes.Post_date}>By: {user_id}</div>
-                    <div className={classes.Post_title}>{title}</div>
-                    <div className={classes.Post_body}>
-                        {body}
-                    </div>
+                <div className={classes.Post_date}>Posted by <Link to={profileLink} className={classes.Post_username}> <span > {user.first_name}</span></Link> at {created_at}
+                    <button className="Button_secondary">delete</button>
                 </div>
+                <div className={classes.Post_title}>{title}</div>
+                <div className={classes.Post_body}>
+                    {body}
+                </div>
+            </div>
         )
     }
 }
